3e13e98f7f3baaa941c85c09912eda21fa7cd510
Jhonata Silva <cicerojhonataoficial@gmail.com>
2024-07-24 19:33:36 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximun-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+    <!-- SLICK CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.css" />
+    <!-- SLICK JS -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick-theme.css"></script>
+
+    <script src="https://kit.fontawesome.com/c5f8abd999.js" crossorigin="anonymous"></script>
+
+    <link href="style.css" rel="stylesheet">
+    <title>Danki Code - Tema</title>
+</head>
+
+<body>
+
+    <section class="top">
+        <div class="center">
+            <header>
+                <div class="logo"><img src="images/logo.png"></div>
+            </header>
+            <ul class="menu-desktop">
+                <li><a href="/index.php">Home</a></li>
+                <li><a href="/sobre.php">Sobre</a></li>
+                <li><a href="/contato.php">Contato</a></li>
+            </ul>
+            <div class="menu-mobile">
+                <i class="fa-solid fa-bars"></i>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </div>
+            <div class="clear"></div>
+            <br /> <br />
+            <div class="w50 time-descricao">
+                <h2>Melhote a comunicação com seu cliente e time</h2>
+                <p>Consultoria especializada em startups, empresas, principalmente pessoas.</p>
+                <a target="_blank" href="https://emanuelagency.com">Ver demonstração</a>
+            </div>
+            <div class="w50 time-imagem">
+                <img src="images/equipe.png" />
+            </div>
+            <div class="clear"></div>
+        </div>
+    </section>
+    <div class="circle">
+        <i class="fa-solid fa-angle-down"></i>
+    </div>
+    <section class="clientes-slider">
+
+        <div class="center">
+            <div style="max-width: 1100px;margin: 0 auto;overflow: hidden;" class="slider-container">
+                <img src="images/amazon.jpg" />
+                <img src="images/costco.jpg" />
+                <img src="images/dominos.jpg" />
+                <img src="images/uber.jpg" />
+                <img src="images/walmart.jpg" />
+                <img src="images/amazon.jpg" />
+                <img src="images/costco.jpg" />
+                <img src="images/dominos.jpg" />
+                <img src="images/uber.jpg" />
+                <img src="images/walmart.jpg" />
+            </div>
+        </div><!--center-->
+    </section>
+
+    <section class="diferenciais">
+        <div class="center">
+            <h2>Contribuímos de ponta a ponta</h2>
+
+            <div class="icons-diferenciais">
+
+                <div class="box-single-diferenciais">
+                    <img src="images\icon1.png">
+                    <h3>Ambientes Mobile</h3>
+                    <p>Garanta que toda sua comunicação esteja alinhada com seu propósito, cada palavra conta.</p>
+                </div><!--box-single-diferenciais-->
+                <div class="box-single-diferenciais">
+                    <img src="images\icon2.png">
+                    <h3>Ambientes Mobile</h3>
+                    <p>Garanta que toda sua comunicação esteja alinhada com seu propósito, cada palavra conta.</p>
+                </div><!--box-single-diferenciais-->
+                <div class="box-single-diferenciais">
+                    <img src="images\icon3.png">
+                    <h3>Ambientes Mobile</h3>
+                    <p>Garanta que toda sua comunicação esteja alinhada com seu propósito, cada palavra conta.</p>
+                </div><!--box-single-diferenciais-->
+            </div>
+        </div>
+    </section>
+
+    <section class="sobre-time">
+        <div class="center">
+            <div class="w50 time-descricao2">
+            <br/><br/><br/><br/>
+                <h2>Um time experiente,<br />comunicador e coeso</h2>
+                <p>A Product Runt acredita que marcas fortes são construídas a partir de transformações importantes e positivas na vida dos consumidores. Exatamente como uma conversa que ganha forma, a ideia vira flâmula.
+                <br /><br />
+                Comunicar bem, de forma coerente e clara não é algo fácil e estamos aqui para ajudar você e seu time. Você escolhe, remoto ou presencial, para seu cliente ou seu público interno, você determina como vamos te ajudar.
+                </p>
+            </div>
+            <div class="w50 img-time">
+                <img src="images\time.png">
+            </div>
+            <div class="clear"></div>
+        </div>
+    </section>
+
+    <section class="depoimentos">
+        <div class="center">
+            <h2>Depoimentos</h2>
+            <div class="depoimentos-box">
+                <div class="depoimentos-single">
+                        <p>"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste distinctio non at quo sint, error laudantium a, perspiciatis explicabo expedita deserunt, eligendi ipsa. Iste alias odit neque culpa eum voluptatem."</p>
+                        <p>Guilherme Grillo</p>
+                        <img src="images/autor.jpg">
+                </div> <!-- Depoimentos Single -->
+                <div class="depoimentos-single">
+                        <p>"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste distinctio non at quo sint, error laudantium a, perspiciatis explicabo expedita deserunt, eligendi ipsa. Iste alias odit neque culpa eum voluptatem."</p>
+                        <p>Guilherme Grillo</p>
+                        <img src="images/autor.jpg">
+                </div> <!-- Depoimentos Single -->
+                <div class="depoimentos-single">
+                        <p>"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste distinctio non at quo sint, error laudantium a, perspiciatis explicabo expedita deserunt, eligendi ipsa. Iste alias odit neque culpa eum voluptatem."</p>
+                        <p>Guilherme Grillo</p>
+                        <img src="images/autor.jpg">
+                </div> <!-- Depoimentos Single -->
+            </div><!-- Depoimentos box-->
+        </div>
+    </section><!-- Depoimentos -->
+
+    <footer>
+	<section class="metodologia">
+		<div class="center">
+			<h2>Conheça nossa Metodologia</h2>
+			<p>O que acha de fazermos o que mais gostamos de fazer? Conversar!<br />Entre em contato por e-mail ou telefone.</p>
+			<a href="/contato">Entrar Contato</a>
+		</div>
+	</section>
+		<div class="center">
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">Contato</a>
+				<a href="">FAQ</a>
+			</div>
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">Contato</a>
+				<a href="">FAQ</a>
+			</div>
+			<div class="col-footer">
+				<h2>Suporte</h2>
+				<a href="">Contato</a>
+				<a href="">FAQ</a>
+			</div>
+			<div style="width: 40%;text-align: right;" class="col-footer">
+				<img src="images/logo-footer.png" />
+			</div>
+		</div>
+	</footer>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script type="text/javascript">
+        $('section.clientes-slider .slider-container').slick({
+            dots: true,
+            arrows: false,
+            infinite: false,
+            speed: 1000,
+            slidesToShow: 4,
+            autoplay: true,
+            centerMode: false,
+            autoplaySpeed: 3000,
+            pauseOnHover: false,
+            responsive: [{
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            }]
+        });
+
+        $('section.depoimentos .depoimentos-box').slick({
+            dots: true,
+            arrows: false,
+            infinite: true,
+            speed: 1000,
+            slidesToShow: 1,
+            autoplay: true,
+            centerMode: false,
+        });
+    </script>
+    <script>
+        //menu responsivo.
+        $(".menu-mobile i").click(function(){
+            $('.menu-mobile').find('ul').slideToggle();
+        })
+    </script>
+</body>
+
+</html>
